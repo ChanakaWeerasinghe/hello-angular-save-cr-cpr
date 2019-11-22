7f26031efc698de5f0dce6c4dc79f4919e414be8
ChanakaWeerasinghe <chanakaweerasinghe92@gmail.com>
2019-11-22 16:44:11 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,24 +30,65 @@ export class AppComponent {
     }
   };
 
+  card = [
+    {
+      cardNumber: "6088 98** **** 2867",
+      expireDate: "9/22",
+      cardHolderName: null,
+      balance: [
+        { currency: "BHD", balance: 10.904, account: "0000035060480000061603" },
+        { currency: "BHD", balance: 20.904, account: "0000035060480000061603" },
+        { currency: "BHD", balance: 30.904, account: "0000035060480000061603" }
+      ],
+      instrumentID: "2305"
+    },
+    {
+      cardNumber: "2222 22** **** 2225",
+      expireDate: "1/3131",
+      cardHolderName: "wrff",
+      balance: [
+        {
+          currency: "BHD",
+          balance: 100.904,
+          account: "0000035060480000061603"
+        },
+        {
+          currency: "BHD",
+          balance: 200.904,
+          account: "0000035060480000061603"
+        },
+        { currency: "BHD", balance: 300.904, account: "0000035060480000061603" }
+      ],
+      instrumentID: "2665"
+    }
+  ];
+
   constructor() {
-    console.log(this.data.biller);
+    if (this.card !== undefined) {
+      let total=0;
+      for (const card of this.card) {
+        for (let i = 0; i < card.balance.length; i++) {
+          console.log("Card balance...", card.balance[i].balance);
+          total= total+card.balance[i].balance;
+        }
+        console.log("Card balance...",total);
+      }
+    }
 
-    if (this.data.biller.value.m5 == "cr") console.log(event);
+    // console.log(this.data.biller);
 
-   
+    // if (this.data.biller.value.m5 == "cr") console.log(event);
   }
 
-  setupSavedBills()
-  {
-     this.cprEvent(event, this.data.biller.value.m5);
+  setupSavedBills() {
+    this.cprEvent(event, this.data.biller.value.m5);
     this.ewaAccNumber = this.data.biller.value.m4;
     console.log("Eva No :", this.ewaAccNumber);
 
     this.ewaAmount = this.data.biller.value.m2;
     console.log("Amount  :", this.ewaAmount);
 
-    this.crCprNumber= this.data.biller.value.m6;
+    this.crCprNumber = this.data.biller.value.m6;
     console.log("crCprNumber  :", this.crCprNumber);
   }
 
